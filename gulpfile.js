bc94caccf5a78ce7fe9be5d92dc842c1b05c6912
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-02-07 22:16:31 +0200

    add gulp plagins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+var {src, dest, watch, series} = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    cleanCSS = require('gulp-clean-css'),
+    minify = require('gulp-minify'),
+    tinypng = require('gulp-tinypng-compress');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
     browserSync.init({
         server: {
             baseDir: "./src/"
@@ -11,4 +14,45 @@ gulp.task('browser-sync', function() {
     gulp.watch("./src/*.html").on('change', browserSync.reload);
     gulp.watch("./src/css/*.css").on('change', browserSync.reload);
     gulp.watch("./src/js/*.js").on('change', browserSync.reload);
-});
+}
+
+// CSS
+function buildCSS(done) {
+    src('./src/css/**.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('./dist/css/'));
+    done();
+}
+
+// JS
+function buildJS(done) {
+    src('./src/js/**.js')
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            }
+        }))
+        .pipe(dest('./dist/js/'));
+    done();
+}
+
+// fonts
+function fonts(done) {
+    src('./src/fonts/**')
+        .pipe(dest('./dist/fonts/'));
+    done();
+}
+
+
+// Image
+function imagemini(done) {
+    src('./src/img/**/**')
+        .pipe(tinypng({
+            key: '3GY9jGdpWdJtkS0gBY34DMPsd5mx5MnS'
+        }))
+        .pipe(dest('./dist/img/'));
+    done();
+}
+
+exports.serve = bs;
+exports.build = series(buildCSS, buildJS, fonts, imagemini);
