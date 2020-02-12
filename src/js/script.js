2a3c5708b6fbea0e7d00d011d8aacc40b3c1b754
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-02-12 16:06:43 +0200

    add section works

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){//страница загрузилась
-    $('.works-block__type').hover(function(){	//при наведении на элемент:
-        var helptext = $(this).attr('data') 	//текст подсказки
-        $('.works-block__tooltip').html(helptext).show();	//размещаем текст подсказки в блок тултипа и делаем его видимым
-        //далее устанавливаем тултипу значения позиции с помощью абсолютного позиционирования:
-        //позиция элемента сверху + высота элемента -> $(this).offset().top+$(this).height()
-        //позиция элемента слева -> $(this).offset().left
-        $('.works-block__tooltip').offset({top:$(this).offset().top+$(this).height(),left:$(this).offset().left});
-    },function(){
-        $('.works-block__tooltip').hide();	//скрываем тултип 
+$(document).ready(function(){
+    toolTiper();
+  });
+  
+  function toolTiper(effect) {
+    $('.tooltiper').each(function(){
+      var eLcontent = $(this).attr('data-tooltip'),
+          eLtop = $(this).position().top,
+          eLleft = $(this).position().left;
+      $(this).append('<span class="tooltip">'+eLcontent+'</span>');
+      var eLtw = $(this).find('.tooltip').width(),
+          eLth = $(this).find('.tooltip').height();
+      $(this).find('.tooltip').css({
+        "top": (0 - eLth - 40)+'px',
+        "left":'-10px'
+      });
     });
-});
+  }
