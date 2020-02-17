0d0f21387af635b2a8310d79134a0d5f826f1f56
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-02-17 21:22:17 +0200

    add services in index.hml

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  // запускаем функцию анимации в нужный момент
+  // файл анимации должен подключаться выше файла, из которого анимация запускается
+  animateForSite()
+})
